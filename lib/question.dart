0702be36b06104c8ac5aca355f1104604c03136e
Jan <janbmagnait@github.com>
2021-09-11 21:12:14 +0800

    39. First Styling & Layouting Steps

--- lib/question.dart
+++ lib/question.dart
@@ -11,7 +11,7 @@ class Question extends StatelessWidget {
       width: double.infinity,
       margin: EdgeInsets.all(10),
       child: Text(
-        questionText,
+        questionTexts,
         style: TextStyle(fontSize: 28),
         textAlign: TextAlign.center,
       ),
