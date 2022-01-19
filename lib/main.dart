912d38ad4476e625108cb0d8ae4277840c75a559
janbmagnait <janbmagnait@yahoo.com>
2022-01-19 16:50:23 +0800

    50 Outputting Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,8 @@ class _MyAppState extends State<MyApp> {
     print(_questionIndex);
     if (_questionIndex < questions.length) {
       print('We have more questions');
+    } else {
+      print('No more questions!');
     }
   }
 
@@ -58,17 +60,21 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex]['questionText'] as String,
-            ),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(
+                    questions[_questionIndex]['questionText'] as String,
+                  ),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                ],
+              )
+            : Center(
+                child: Text('You did it!'),
+              ),
       ),
     );
   }
